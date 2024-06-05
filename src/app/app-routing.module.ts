f44b82c5529fc157b7e63162fcaa675d971c870b
Kishor S <kishorsbca@gmail.com>
2024-06-05 15:19:41 +0530

    changing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { PlanComponent } from './plan/plan.component'; 
-import { PlanningComponent} from './planning/planning.component'
+import { PlanComponent } from './plan/plan.component';
+import { PlanningComponent } from './planning/planning.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home',component:HomeComponent},
-  {path:'contactus',component:ContactUsComponent},
-  {path:'aboutus',component:AboutUsComponent},
-  {path:'plan',component:PlanComponent},
-  {path:'planning',component:PlanningComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'plan', component: PlanComponent },
+  { path: 'planning', component: PlanningComponent },
+  { path: 'ang-app-2', component: HomeComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
